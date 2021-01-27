c10211f48fdb22bb766a5c218b97ae056eaf8872
Jaime Mas <sanmasja@gmail.com>
2021-01-27 11:46:35 +0100

    Mensaje de referencia

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre = 'Jaime';
+let nombre2: string ='Jaime';
+const nombre3 = 'Jaime';
+
+let edad: number | string = 'adulto';
+
+console.log( nombre, edad )
